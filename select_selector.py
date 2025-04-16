b9e2cd84831ef54c6e85837e8d01751409bbed0e
Mr7F <git@sep.tf>
2025-04-16 20:35:11 +0200

    [FIX] select empty string

--- select_selector.py
+++ select_selector.py
@@ -3,7 +3,8 @@ import sublime_plugin
 
 from .utils import select_next_region
 
-_string_selector = "((meta.string | string.quoted.double.json) - punctuation.definition.string.begin - punctuation.definition.string.end) | meta.interpolation"
+_string_with_quote = "meta.string | string.quoted.double.json"
+_string_selector = f"(({_string_with_quote}) - punctuation.definition.string.begin - punctuation.definition.string.end) | meta.interpolation"
 
 
 class JumperSelectSelectorCommand(sublime_plugin.TextCommand):
@@ -13,7 +14,24 @@ class JumperSelectSelectorCommand(sublime_plugin.TextCommand):
     > https://www.sublimetext.com/docs/selectors.html
     """
 
-    def run(self, edit, direction="next", selector=_string_selector, extend=False):
-        strings = self.view.find_by_selector(selector)
+    def run(self, edit, direction="next", selector=None, extend=False):
+        if selector is None:
+            strings = self.view.find_by_selector(_string_selector)
+
+            # Find empty strings
+            all_strings = self.view.find_by_selector(_string_with_quote)
+            strings += [
+                sublime.Region(r.a + 1, r.b - 1) for r in all_strings if len(r) == 2
+            ]
+            # Triple quote empty in python
+            strings += [
+                sublime.Region(r.a + 3, r.b - 3)
+                for r in all_strings
+                if len(r) == 6
+                and self.view.match_selector(r.a, "meta.string.python")
+                and self.view.substr(r) in ('"' * 6, "'" * 6)
+            ]
+        else:
+            strings = self.view.find_by_selector(selector)
 
         select_next_region(self.view, strings, direction, extend)
